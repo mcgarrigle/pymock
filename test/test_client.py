e02168e893c08bece1ed7609187456902e283146
Peter McGarrigle <peter.mcgarrigle@bt.com>
2017-10-19 08:43:24 +0000

    tidy up

--- test/test_client.py
+++ test/test_client.py
@@ -15,12 +15,8 @@ class ClientTest(unittest.TestCase):
 
     def test_service_gets_called(self):
         Client(self.mock_service).run() 
-        #print(self.mock_service.send)
-        #print(self.mock_service.mock_calls[0])
         print(self.mock_service.send.mock_calls)
-        print(self.mock_service.mock_calls)
         self.mock_service.send.assert_called_once_with("hello")
-        #self.mock_service.send.assert_called
 
 if __name__ == '__main__':
     unittest.main()        
